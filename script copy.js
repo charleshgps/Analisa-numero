c3f04daf46b093c74489c55b1ac50215fbcaf547
Charles Silva <42080053+charleshgps@users.noreply.github.com>
2023-01-30 14:13:21 -0300

    Create script copy.js

---
+++ script copy.js
@@ -0,0 +1,9 @@
+function find_max(nums) {
+    let max_num = Number.NEGATIVE_INFINITY; // smaller than all other numbers
+    for (let num of nums) {
+        if (num > max_num) {
+            // (Fill in the missing line here)
+        }
+    }
+    return max_num;
+}
